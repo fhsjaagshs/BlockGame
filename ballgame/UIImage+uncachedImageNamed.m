6409707c261a5ddea08050c6b19001236097b11f
Nathaniel Symer <nate@natesymer.com>
2013-05-18 18:34:45 -0400

    Some final changes

--- ballgame/UIImage+uncachedImageNamed.m
+++ ballgame/UIImage+uncachedImageNamed.m
@@ -11,24 +11,37 @@
 @implementation UIImage (uncachedImageNamed)
 
 + (UIImage *)uncachedImageNamed:(NSString *)imageName {
-    float scale = [[UIScreen mainScreen]scale];
     NSString *name = nil;
-    
+
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-        if (scale == 2) {
+        if ([[UIScreen mainScreen]scale] == 2) {
             name = [imageName stringByAppendingString:@"~iPad@2x"];
             if ([[NSBundle mainBundle]pathForResource:name ofType:@"png"].length == 0) {
                 name = [imageName stringByAppendingString:@"-iPad@2x"];
+                
+                if ([[NSBundle mainBundle]pathForResource:name ofType:@"png"].length == 0) {
+                    name = [imageName stringByAppendingString:@"~iPad"];
+                    if ([[NSBundle mainBundle]pathForResource:name ofType:@"png"].length == 0) {
+                        name = [imageName stringByAppendingString:@"-iPad"];
+                        if ([[NSBundle mainBundle]pathForResource:name ofType:@"png"].length == 0) {
+                            name = imageName;
+                        }
+                    }
+                }
             }
         } else {
             name = [imageName stringByAppendingString:@"~iPad"];
             if ([[NSBundle mainBundle]pathForResource:name ofType:@"png"].length == 0) {
                 name = [imageName stringByAppendingString:@"-iPad"];
+                
+                if ([[NSBundle mainBundle]pathForResource:name ofType:@"png"].length == 0) {
+                    name = imageName;
+                }
             }
         }
     } else {
-        if (scale == 2) {
-            if ([[UIScreen mainScreen]bounds].size.height*scale >= 1136) {
+        if ([[UIScreen mainScreen]scale] == 2) {
+            if ([[UIScreen mainScreen]bounds].size.height*2 >= 1136) {
                 name = [imageName stringByAppendingString:@"-568h@2x"];
                 if ([[NSBundle mainBundle]pathForResource:name ofType:@"png"].length == 0) {
                     
@@ -43,14 +56,16 @@
                 }
             } else {
                 name = [imageName stringByAppendingString:@"@2x"];
+                if ([[NSBundle mainBundle]pathForResource:name ofType:@"png"].length == 0) {
+                    name = imageName;
+                }
             }
         } else {
             name = imageName;
         }
     }
     
-    NSString *path = [[NSBundle mainBundle]pathForResource:name ofType:@"png"];
-    return [UIImage imageWithContentsOfFile:path];
+    return [UIImage imageWithContentsOfFile:[[NSBundle mainBundle]pathForResource:name ofType:@"png"]];
 }
 
 @end
